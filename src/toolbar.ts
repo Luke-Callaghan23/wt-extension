8e7d497622c5dc10a2723424de5ecd064ca7969e
Luke Callaghan <luke.callaghan23@gmail.com>
2023-10-23 15:55:21 -0400

    #115

--- src/toolbar.ts
+++ src/toolbar.ts
@@ -333,8 +333,8 @@ async function jumpSentence (jt: JumpType, shiftHeld: boolean, jumpFragment: boo
 
     
     const fragmentStop = jumpFragment 
-        ? /["\-,;\*#~_]/                    // If fragment jumps are activated, use all stopping characters as pause for fragments
-        : /^[]/;                            // Otherwise, use a regex that the internet told me would never, ever (tm) match anything
+        ? /["\-,;\*#~_()\[\]\{\}]/                    // If fragment jumps are activated, use all stopping characters as pause for fragments
+        : /^[]/;                                      // Otherwise, use a regex that the internet told me would never, ever (tm) match anything
     
     const startOffset = document.offsetAt(start);
 
