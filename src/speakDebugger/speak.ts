5d5389138feb48e1c1dd01af1e57ae921fab42c8
Luke Callaghan <luke.callaghan23@gmail.com>
2024-04-05 12:03:24 -0400

    init2

---
+++ src/speakDebugger/speak.ts
@@ -0,0 +1,64 @@
+import * as say from 'say';
+import * as vscode from 'vscode';
+
+const getVoice = (): string | undefined =>
+    vscode.workspace.getConfiguration('speech').get<string>('voice');
+
+const getSpeed = (): number | undefined =>
+    vscode.workspace.getConfiguration('speech').get<number>('speed');
+
+const getSubstitutions = (): { [key: string]: string } =>
+    vscode.workspace.getConfiguration('speech').get<{ [key: string]: string }>('substitutions') || {};
+
+
+const stopSpeaking = () => {
+    say.stop();
+}
+
+const cleanText = (text: string): string => {
+    text = text.trim();
+    for (let [pattern, replacement] of Object.entries(getSubstitutions())) {
+        text = text.replaceAll(pattern, replacement);
+    }
+    return text;
+}
+
+const speakText = (text: string) => {
+    text = cleanText(text);
+    if (text.length > 0) {
+        say.speak(text, getVoice(), getSpeed());
+    }
+};
+
+const speakCurrentSelection = (editor: vscode.TextEditor) => {
+    const selection = editor.selection;
+    if (!selection)
+        return;
+
+    speakText(editor.document.getText(selection));
+};
+
+const speakDocument = (editor: vscode.TextEditor) => {
+    speakText(editor.document.getText());
+};
+
+
+export function activateSpeak(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('speech.speakDocument', (editor) => {
+        stopSpeaking();
+        if (!editor)
+            return;
+        speakDocument(editor);
+    }));
+
+    context.subscriptions.push(vscode.commands.registerTextEditorCommand('speech.speakSelection', (editor) => {
+        stopSpeaking();
+        if (!editor)
+            return;
+        speakCurrentSelection(editor);
+    }));
+
+    context.subscriptions.push(vscode.commands.registerCommand('speech.stopSpeaking', () => {
+        stopSpeaking();
+    }));
+}
