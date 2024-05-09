c830998e5da4dddac69ac40c918e4486b2faa13b
Luke Callaghan <luke.callaghan23@gmail.com>
2024-05-08 20:07:08 -0400

    #210

--- src/codeMode/exitCodeMode.ts
+++ src/codeMode/exitCodeMode.ts
@@ -37,11 +37,20 @@ export async function exit (this: CoderModer): Promise<void> {
     this.openedExplorer = false;
     this.openedOutput = false;
 
-    // Sometimes on slower machines, exiting code mode deactivates all the timed higlighting and stuff until you 
-    //      switch to a new tab -- no clue why -- so by opening a new tab and then closing it immediately we can
-    //      get around this
-    const result = await vscode.commands.executeCommand('workbench.action.files.newUntitledFile');
-    await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+    
+    const settingsEnabled = vscode.workspace.getConfiguration(`wt.codeMode`);
+    const enabled = settingsEnabled
+        ? !!settingsEnabled.get<boolean>("slowMode")
+        : true;
+
+    if (enabled) {
+        // Sometimes on slower machines, exiting code mode deactivates all the timed higlighting and stuff until you 
+        //      switch to a new tab -- no clue why -- so by opening a new tab and then closing it immediately we can
+        //      get around this
+        const result = await vscode.commands.executeCommand('workbench.action.files.newUntitledFile');
+        await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+    }
+
 
     
     await TabLabels.assignNamesForOpenTabs();
