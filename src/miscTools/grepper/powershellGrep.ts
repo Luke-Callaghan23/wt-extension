78942b06d7bb9b3f169ffba7b05c8758bf8655fa
Luke Callaghan <luke.callaghan23@gmail.com>
2025-06-13 13:38:43 -0400

    fixed powershell grep

--- src/miscTools/grepper/powershellGrep.ts
+++ src/miscTools/grepper/powershellGrep.ts
@@ -21,33 +21,19 @@ export async function *powershellGrep (
         }
     }
 
-    // Output of a git grep command is of this format:
-    // URI:ONE_INDEXED_LINE:CONTENTS_OF_LINE
-    // This is not particularly helpful to us because if we want to generate vscode.Location objects
-    //      we are missing the start and end range of the matched text
-    // This function mainly exists to process the raw output of a git grep command output and 
-    //      transform it into vscode.Location so it can be used elsewhere in the writing environment
-
-
     if (!useRegex) {
-        // If the searchBarValue is not a regex, then we have to comment out all the regex characters
-        //      inside of the text, as git grep and the rest of this function will assume searchBarValue
-        //      is the text of a regex
-        // This essentially "turns off" all the potential regex characters in the text and lets the 
-        //      rest of the code pretend that searchBarValue is a regex
         searchBarValue = searchBarValue.replaceAll(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 
     if (wholeWord) {
-        
-
         // Basically a git grep command requires a very specific formatting for the special characters in a regex
         // So, we cannot rely on existing word separators that have been declared elsewhere in this project
         // Have to recreate the regex using these special word separators
-        const shellWordSeparatorStart = '(^|\\s|-|[.?:;,()!&"\'^_*~])';
-        const shellWordSeparatorEnd = '(\\s|-|[.?:;,()!&"\'^_*~]|$)';
+        const shellWordSeparatorStart = '(^|\\s|-|[.?:;,()\\!\\&\\"\'^_*~])';
+        const shellWordSeparatorEnd = '(\\s|-|[.?:;,()\\!\\&\\"\'^_*~]|$)';
         searchBarValue = `${shellWordSeparatorStart}${searchBarValue}${shellWordSeparatorEnd}`;
     }
+
     
     const caseSensitive: string[] | string = !caseInsensitive
         ? '-CaseSensitive'
@@ -64,10 +50,50 @@ export async function *powershellGrep (
         const ps = childProcess.spawn('powershell.exe', args, {
             cwd: extension.rootPath.fsPath
         });
+        
+        if (ps.pid) {
+            runningGreps[ps.pid] = ps;
+        }
+
+        for await (const line of readline.createInterface({ input: ps.stderr })) {
+            console.error(line);
+            console.error(line);
+
+            console.error(line);
+            console.error(line);
+            console.error(line);
+
+            console.error(line);
+            console.error(line);
+            console.error(line);
+
+            console.error(line);
+            console.error(line);
+            console.error(line);
+
+            console.error(line);
+            console.error(line);
+            console.error(line);
+
+            console.error(line);
+        }
+
+        cancellationToken.onCancellationRequested(() => {
+            if (ps.pid && ps.pid in runningGreps) {
+                delete runningGreps[ps.pid];
+            }
+            ps.kill();
+            cancelled = true;
+        })
+
         // Any "finished" operation for the grep command should reset the git state back to its original
         // Iterate over lines from the stdout of the git grep command and yield each line provided to us
         for await (const line of readline.createInterface({ input: ps.stdout })) {
             yield line.toLocaleLowerCase().replaceAll(extension.rootPath.fsPath.toLocaleLowerCase(), '');
+        }
+
+        if (ps.pid && ps.pid in runningGreps) {
+            delete runningGreps[ps.pid];
         }
     }
     catch (err: any) {
