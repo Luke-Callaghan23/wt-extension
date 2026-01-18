f7e8aba3105301eda4a7aa5506b7e356a0522a5e
Luke Callaghan <luke.callaghan23@gmail.com>
2026-01-17 23:30:16 -0500

    #457

--- src/gitTransactions.ts
+++ src/gitTransactions.ts
@@ -13,11 +13,24 @@ export async function gitiniter () {
     }
 }
 
+
+const commitMessage = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+);
+commitMessage.backgroundColor = new vscode.ThemeColor('statusBarItem.warningBackground');
+commitMessage.text = `Committing All Files $(loading~spin)`;
+commitMessage.tooltip = new vscode.MarkdownString(`
+### (WTANIWE) Saving all recently updated files, including configuration files and modified text, to git
+- Write your commit message on the first line of the commit editor.
+- Hit "Commit" in the bottom right hand corner.
+- Then you can sync to an external repository or keep your work local.`
+);
+
 export async function gitCommit () {
     try {
-        return defaultProgress("Commiting All Files", async () => {
-            await vscode.commands.executeCommand('git.commitAll');
-        });
+        commitMessage.show();
+        await vscode.commands.executeCommand('git.commitAll');
+        commitMessage.hide()
     }
     catch (e) {
         vscode.window.showErrorMessage(`ERROR: An error occurred while making commit: ${e}`);
@@ -27,10 +40,10 @@ export async function gitCommit () {
 
 export async function gitCommitFile () {
     try {
-        return defaultProgress("Commiting All Files", async () => {
-            await vscode.commands.executeCommand('git.stage');
-            await vscode.commands.executeCommand('git.commit');
-        });
+        commitMessage.show();
+        await vscode.commands.executeCommand('git.stage');
+        await vscode.commands.executeCommand('git.commit');
+        commitMessage.hide();
     }
     catch (e) {
         vscode.window.showErrorMessage(`ERROR: An error occurred while making commit: ${e}`);
